268a08a94f5e8efe2ef45aa0a86d76fe4336d4ff
Bachtiar <bahtiarvivo123@gmail.com>
2026-03-02 18:37:11 +0700

    Tugas LKP 2

--- app/src/main/java/com/example/praktam_2417051058/MainActivity.kt
+++ app/src/main/java/com/example/praktam_2417051058/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.praktam_2417051058
 
-import Model.KucingSource
+import Model.KegiatanSource
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.praktam_2417051058.ui.theme.PrakTAM_2417051058Theme
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.verticalScroll
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,8 +30,6 @@ class MainActivity : ComponentActivity() {
             PrakTAM_2417051058Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Bachtiar Nugraha",
-                        npm = "2417051058",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -35,13 +39,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, npm: String, modifier: Modifier = Modifier) {
-    val kucing = KucingSource.dummyKucing[0]
+fun Greeting(modifier: Modifier = Modifier) {
+    val scrollState = rememberScrollState()
 
-    Column(modifier = Modifier.fillMaxSize().padding(all = 30.dp)) {
-        Text(text = "Nama : ${kucing.nama}")
-        Text(text = "Warna Bulu : ${kucing.warna}")
-        Text(text = "Tahun Lahir : ${kucing.tahunLahir}")
+    Column(modifier = Modifier.fillMaxSize().padding(all = 30.dp).verticalScroll(scrollState)) {
+        val scrollState = rememberScrollState()
+
+        KegiatanSource.dummyKegiatan.forEach { kegiatan ->
+            Image(
+                painter = painterResource(id = kegiatan.ImageRes),
+                contentDescription = kegiatan.namaKegiatan,
+                modifier = Modifier.size(200.dp),
+                contentScale = ContentScale.Crop
+            )
+
+            Text(text = "Nama Kegiatan : ${kegiatan.namaKegiatan}")
+            Text(text = "Deskripsi Kegiatan : ${kegiatan.deskripsi}")
+            Text(text = "Waktu Kegiatan : ${kegiatan.waktu}")
+        }
     }
 }
 
@@ -49,6 +64,6 @@ fun Greeting(name: String, npm: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PrakTAM_2417051058Theme {
-        Greeting(name = "Bachtiar Nugraha", npm = "2417051058")
+        Greeting()
     }
 }
