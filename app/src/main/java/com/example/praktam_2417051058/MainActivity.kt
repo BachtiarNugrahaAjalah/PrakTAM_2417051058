5e6963a8c61f6365fe4e3d0473ff322f8bf30c08
Bachtiar <bahtiarvivo123@gmail.com>
2026-03-12 22:41:43 +0700

    Tugas LKP 3

--- app/src/main/java/com/example/praktam_2417051058/MainActivity.kt
+++ app/src/main/java/com/example/praktam_2417051058/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.praktam_2417051058
 
+import Model.Kegiatan
 import Model.KegiatanSource
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -21,6 +24,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.praktam_2417051058.ui.theme.PrakTAM_2417051058Theme
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,34 +38,72 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PrakTAM_2417051058Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DaftarKegiatanScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
+fun DaftarKegiatanScreen(modifier: Modifier = Modifier) {
     val scrollState = rememberScrollState()
 
-    Column(modifier = Modifier.fillMaxSize().padding(all = 30.dp).verticalScroll(scrollState)) {
-        val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+            .verticalScroll(rememberScrollState())) {
 
         KegiatanSource.dummyKegiatan.forEach { kegiatan ->
+            DetailScreen(kegiatan = kegiatan)
+            Spacer(modifier = Modifier.height(24.dp))
+        }
+    }
+}
+
+@Composable
+fun DetailScreen(kegiatan: Kegiatan){
+    Card(modifier = Modifier.padding(16.dp)) {
+        Column(modifier = Modifier.fillMaxWidth().padding(12.dp)) {
             Image(
                 painter = painterResource(id = kegiatan.ImageRes),
                 contentDescription = kegiatan.namaKegiatan,
-                modifier = Modifier.size(200.dp),
-                contentScale = ContentScale.Crop
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Fit
             )
 
-            Text(text = "Nama Kegiatan : ${kegiatan.namaKegiatan}")
-            Text(text = "Deskripsi Kegiatan : ${kegiatan.deskripsi}")
-            Text(text = "Waktu Kegiatan : ${kegiatan.waktu}")
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = kegiatan.namaKegiatan,
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = kegiatan.deskripsi,
+                style = MaterialTheme.typography.bodyLarge
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Waktu : ${kegiatan.waktu}",
+                style = MaterialTheme.typography.bodyLarge
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Lakukan Sekarang")
+            }
         }
     }
 }
@@ -64,6 +112,6 @@ fun Greeting(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PrakTAM_2417051058Theme {
-        Greeting()
+        DaftarKegiatanScreen()
     }
 }
