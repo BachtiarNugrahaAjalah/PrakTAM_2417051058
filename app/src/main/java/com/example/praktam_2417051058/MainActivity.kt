ba4658c87949063b0e903deed16074abfa7522cd
Bachtiar <bahtiarvivo123@gmail.com>
2026-03-18 22:00:38 +0700

    Tugas LKP 4

--- app/src/main/java/com/example/praktam_2417051058/MainActivity.kt
+++ app/src/main/java/com/example/praktam_2417051058/MainActivity.kt
@@ -2,14 +2,20 @@ package com.example.praktam_2417051058
 
 import Model.Kegiatan
 import Model.KegiatanSource
+import com.example.praktam_2417051058.ui.theme.PurpleGrey40
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -23,14 +29,27 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.praktam_2417051058.ui.theme.PrakTAM_2417051058Theme
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,71 +57,115 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PrakTAM_2417051058Theme {
-                DaftarKegiatanScreen()
+//                DaftarKegiatanScreen()
+                DashboardSection()
             }
         }
     }
 }
 
 @Composable
-fun DaftarKegiatanScreen(modifier: Modifier = Modifier) {
+fun HeaderSection(){
+    Column(modifier = Modifier.fillMaxWidth().background(PurpleGrey40, shape = RoundedCornerShape(5.dp) ).padding(20.dp)) {
+        Text(
+            text = "Hai, Pengguna",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        Text("Selamat Datang di LifePattern", color = Color.White)
+    }
+}
+
+@Composable
+fun DashboardSection(){
     val scrollState = rememberScrollState()
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
+    ){
+        Column(
+            modifier = Modifier
+                .padding(0.dp, 10.dp, 0.dp, 0.dp)
+                .fillMaxWidth()
+                .align(Alignment.TopCenter)
+        ) {
+            HeaderSection()
+            DaftarKegiatanScreen()
+        }
+    }
+}
+
+@Composable
+fun DaftarKegiatanScreen() {
 
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(24.dp)
-            .verticalScroll(rememberScrollState())) {
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
 
         KegiatanSource.dummyKegiatan.forEach { kegiatan ->
             DetailScreen(kegiatan = kegiatan)
-            Spacer(modifier = Modifier.height(24.dp))
+            Spacer(modifier = Modifier.height(2.dp))
         }
     }
 }
 
 @Composable
 fun DetailScreen(kegiatan: Kegiatan){
-    Card(modifier = Modifier.padding(16.dp)) {
-        Column(modifier = Modifier.fillMaxWidth().padding(12.dp)) {
-            Image(
-                painter = painterResource(id = kegiatan.ImageRes),
-                contentDescription = kegiatan.namaKegiatan,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp),
-                contentScale = ContentScale.Fit
-            )
+    var isDoing by remember {mutableStateOf(false)}
+    Card(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+        Row(modifier = Modifier.fillMaxWidth().padding(12.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+            Box(modifier = Modifier){
+                Image(
+                    painter = painterResource(id = kegiatan.ImageRes),
+                    contentDescription = kegiatan.namaKegiatan,
+                    modifier = Modifier
+                        .height(80.dp)
+                        .width(80.dp),
+                    contentScale = ContentScale.Fit
+                )
+            }
 
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.width(10.dp))
 
-            Text(
-                text = kegiatan.namaKegiatan,
-                style = MaterialTheme.typography.headlineMedium,
-                fontWeight = FontWeight.Bold
-            )
+            Column() {
+                Text(
+                    text = kegiatan.namaKegiatan,
+                    style = MaterialTheme.typography.headlineLarge,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+                
+                Text(
+                    text = "Waktu : ${kegiatan.waktu}",
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontSize = 14.sp
+                )
 
-            Spacer(modifier = Modifier.height(8.dp))
+                Button(
+                    onClick = { },
+                    modifier = Modifier
+                ) {
+                    Text("Detail Kegiatan")
+                }
+            }
 
-            Text(
-                text = kegiatan.deskripsi,
-                style = MaterialTheme.typography.bodyLarge
-            )
-
-            Spacer(modifier = Modifier.height(8.dp))
-
-            Text(
-                text = "Waktu : ${kegiatan.waktu}",
-                style = MaterialTheme.typography.bodyLarge
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Button(
-                onClick = { },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("Lakukan Sekarang")
+            Column(modifier = Modifier.fillMaxHeight()) {
+                IconButton(
+                    onClick = {isDoing = !isDoing},
+                    modifier = Modifier
+                ) {
+                    Icon(
+                        imageVector = if(isDoing) Icons.Filled.CheckCircle else Icons.Outlined.CheckCircle,
+                        contentDescription = "Doing icon",
+                        tint = if (isDoing) Color.Green else Color.Black
+                    )
+                }
             }
         }
     }
@@ -112,6 +175,7 @@ fun DetailScreen(kegiatan: Kegiatan){
 @Composable
 fun GreetingPreview() {
     PrakTAM_2417051058Theme {
-        DaftarKegiatanScreen()
+//        DaftarKegiatanScreen()
+        DashboardSection()
     }
 }
